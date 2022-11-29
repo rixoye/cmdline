0d1c516898b6b4c63766822bba6d526ba3b4660e
rixojojo <rixojojo@local.com>
2022-11-29 16:29:57 +0800

    根据issue信息，增加了windows下的兼容性

--- cmdline.h
+++ cmdline.h
@@ -36,8 +36,34 @@
 #include <typeinfo>
 #include <cstring>
 #include <algorithm>
-#include <cxxabi.h>
 #include <cstdlib>
+
+#ifndef WIN32
+#include <cxxabi.h>
+
+static inline std::string demangle(const std::string& name)
+{
+    int status = 0;
+    char* p = abi::__cxa_demangle(name.c_str(), 0, 0, &status);
+    std::string ret(p);
+    free(p);
+    return ret;
+}
+#else
+
+#include <windows.h> 
+#include <Dbghelp.h>
+#pragma comment(lib,"dbghelp.lib")     
+
+static inline std::string demangle(const std::string& name)
+{
+    TCHAR szUndecorateName[256];
+    memset(szUndecorateName, 0, 256);
+    UnDecorateSymbolName(name.c_str(), szUndecorateName, 256, 0);
+    return szUndecorateName;
+}
+#endif
+
 
 namespace cmdline{
 
@@ -102,14 +128,6 @@ Target lexical_cast(const Source &arg)
   return lexical_cast_t<Target, Source, detail::is_same<Target, Source>::value>::cast(arg);
 }
 
-static inline std::string demangle(const std::string &name)
-{
-  int status=0;
-  char *p=abi::__cxa_demangle(name.c_str(), 0, 0, &status);
-  std::string ret(p);
-  free(p);
-  return ret;
-}
 
 template <class T>
 std::string readable_typename()
@@ -566,7 +584,7 @@ public:
 
     size_t max_width=0;
     for (size_t i=0; i<ordered.size(); i++){
-      max_width=std::max(max_width, ordered[i]->name().length());
+      max_width=(std::max)(max_width, ordered[i]->name().length());
     }
     for (size_t i=0; i<ordered.size(); i++){
       if (ordered[i]->short_name()){
